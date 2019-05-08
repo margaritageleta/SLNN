a2259d0210bcda126ac9b11caad55f2dd6f04fe3
Ibrar Malik <9162284+ibrarmalik@users.noreply.github.com>
2019-05-08 21:59:08 +0200

    Add CV and change linesearch

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ This file can also be used as a python module.
 """
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-from scipy.optimize.linesearch import line_search
+from scipy.optimize import line_search
 
 
 # -- Image utils --
@@ -101,37 +102,108 @@ def y(X, w):
 
 
 def loss(w, X, ytr, p=0):
-    #return np.sum((y(X, w) - ytr)**2) + p/2 * np.sum(w**2)
+    # return np.sum((y(X, w) - ytr)**2) + p/2 * np.sum(w**2)
     return np.linalg.norm(y(X, w) - ytr)**2 + p/2 * np.linalg.norm(w)**2
 
 
 def g_loss(w, X, ytr, p=0):
-    #return (2 * sigmoid(X) * ((y(X, w) - ytr) * y(X, w) * (1 - y(X, w))) + p*w).sum(axis=0)
+    # return (2 * sigmoid(X) * ((y(X, w) - ytr) * y(X, w) * (1 - y(X, w))) + p*w).sum(axis=0)
     return np.squeeze(2 * sigmoid(X.T) @ ((y(X, w) - ytr) * y(X, w) * (1 - y(X, w))) + p*w.T)
+
 
 # -- Optimization --
 
-
-def GM(x, f, g, eps, kmax):
+def GM(x, f, g, eps, kmax, precision=6):
+    gradient_norm = np.round(np.linalg.norm(g(x)), precision)
+    Xk = [[np.NaN, f(x), gradient_norm]]
+    rk = [np.NaN]
+    Mk = [np.NaN]
+    # ============== #
     k = 0
     while np.linalg.norm(g(x)) > eps and k < kmax:
         d = -g(x)
-        alpha, *_ = line_search(f, g, x, d)
-        x = x + alpha*d
+        if k > 0:
+            alpha, *_ = line_search(f, g, x, d, old_old_fval=f(x_prev), c1=0.01, c2=0.45)
+        else:
+            alpha, *_ = line_search(f, g, x, d, c1=0.01, c2=0.45)
+        if alpha is None:
+            break
+        x, x_prev = x + alpha*d, x
         k += 1
+        # =========== #
+        gradient_norm = np.round(np.linalg.norm(g(x)), precision)
+        Xk.append([alpha, f(x), gradient_norm])
+        rk.append(np.linalg.norm(g(x))/np.linalg.norm((g(x_prev))))
+        Mk.append(np.linalg.norm(g(x))/(np.linalg.norm((g(x_prev)))**2))
+        # =========== #
+    data = pd.DataFrame(Xk, columns=["alpha", "f(x)", "||g(x)||"], dtype=np.float)
+    data['r'] = rk
+    data['M'] = Mk
+    return x, data
 
-    return x
+
+def CGM(x, f, g, eps, kmax, iCG, iRC, nu=None, precision=6):
+    gradient_norm = round(np.linalg.norm(g(x)), precision)
+    Xk = [[np.NaN, f(x), gradient_norm]]
+    rk = [np.NaN]
+    Mk = [np.NaN]
+    # ============== #
+    d = -g(x)
+    k = 0
+    while np.linalg.norm(g(x)) > eps and k < kmax:
+        if k > 0:
+            alpha, *_ = line_search(f, g, x, d, old_old_fval=f(x_prev), c1=0.01, c2=0.45)
+        else:
+            alpha, *_ = line_search(f, g, x, d, c1=0.01, c2=0.45)
+        if alpha is None:
+            break
+        x, x_prev = x + alpha*d, x
+        # =========== #
+        # CGM variants
+        if iCG == "FR":
+            beta = (g(x).T @ g(x)) / (g(x_prev).T @ g(x_prev))
+        elif iCG == "PR":
+            beta = max(0, g(x).T @ (g(x) - g(x_prev)) / (g(x_prev).T @ g(x_prev)))
+        else:
+            raise TypeError("iCG should be FR (Fletcher-Reeves) or PR (Polak-Ribière)")
+        # Restart conditions
+        if iRC > 0 and nu is None:
+            raise TypeError(f"nu is a necessary parameter with iRC equal to {iRC}")
+        if (iRC == 1 and k % nu == 0 or
+                iRC == 2 and g(x).T @ g(x_prev) / np.linalg.norm(g(x))**2 > nu or
+                k == 0):
+            d = -g(x)
+        else:
+            d = -g(x) + beta*d
+        k += 1
+        # =========== #
+        gradient_norm = np.round(np.linalg.norm(g(x)), precision)
+        Xk.append([alpha, f(x), gradient_norm])
+        rk.append(np.linalg.norm(g(x))/np.linalg.norm((g(x_prev))))
+        Mk.append(np.linalg.norm(g(x))/(np.linalg.norm((g(x_prev)))**2))
+        # =========== #
+    data = pd.DataFrame(Xk, columns=["alpha", "f(x)", "||g(x)||"], dtype=np.float)
+    data['r'] = rk
+    data['M'] = Mk
+    return x, data
 
 
-def BFGS(x, f, g, eps, kmax):
+def BFGS(x, f, g, eps, kmax, precision=6):
+    gradient_norm = np.round(np.linalg.norm(g(x)), precision)
+    Xk = [[np.NaN, f(x), gradient_norm]]
+    rk = [np.NaN]
+    Mk = [np.NaN]
+    # =========== #
     H = I = np.identity(len(g(x)))
     k = 0
     while np.linalg.norm(g(x)) > eps and k < kmax:
         d = -H @ g(x)
-        alpha, *_ = line_search(f, g, x, d, c1 = 0.01, c2 = 0.45)
-        print(alpha)
+        if k > 0:
+            alpha, *_ = line_search(f, g, x, d, old_old_fval=f(x_prev), c1=0.01, c2=0.45)
+        else:
+            alpha, *_ = line_search(f, g, x, d, c1=0.01, c2=0.45)
         if alpha is None:
-            return x
+            break
         x, x_prev = x + alpha*d, x
         s = x - x_prev
         y = g(x) - g(x_prev)
@@ -139,46 +211,78 @@ def BFGS(x, f, g, eps, kmax):
         rho = 1 / ((y).T @ s)
         H = (I - rho * s @ y.T) @ H @ (I - rho * y @ (s.T)) + rho * s @ s.T
         k += 1
-    return x
+        # =========== #
+        gradient_norm = np.round(np.linalg.norm(g(x)), precision)
+        Xk.append([alpha, f(x), gradient_norm])
+        rk.append(np.linalg.norm(g(x))/np.linalg.norm((g(x_prev))))
+        Mk.append(np.linalg.norm(g(x))/(np.linalg.norm((g(x_prev)))**2))
+        # =========== #
+    data = pd.DataFrame(Xk, columns=["alpha", "f(x)", "||g(x)||"], dtype=np.float)
+    data['r'] = rk
+    data['M'] = Mk
+    return x, data
+
+# -- SLNN class --
 
 
-"""
- BLS params:
- ----------
-         kmaxBLS = 30, 
-         epsilonBLS = 1.0e-03, 
-         c1 = 0.01, 
-         c2 = 0.45
-         
- optimizer:
- ---------
-         Steepest descent (GM)
-         Conjugate Gradient method (CGM)
-         Quasi-Newton BFGS
-""" 
+class SLNN:
+    def __init__(self, n=35):
+        self.weights = np.zeros((1, 35))
+        self._trained = False
+        self.out = None
 
-# def nnet(Xtr, Ytr,lambda = 0.00, epsilon = 1.0e-06, kmax = 500, BLS_params, optimizer):
-#     # init rand weights
-#     w = rand_weights
-    
-#     if optimizer == "GM": # Steepest descent
-#         objective_func = loss(w, Xtr, Ytr, p = lambda)
-#         GM(x, f, , BLS_params, eps = epsilon, kmax = kmax)
-#         return 0
-#     ##
-#     elif optimizer == "CGM": # Conjugate Gradient method
-        
-#         return 1
-#     ##
-#     elif optimizer == "BFGS": # Quasi-Newton BFGS
-        
-#         return 2
-#     ##
-#     else:
-#         print("Invalid optimizer.")
-        
-    
+    def train(self, optimizer, x, y, p = 0, epsilon=10e-6, kmax = 1000):
+        if optimizer == "GM":
+            self.weights, self.out = GM(self.weights, lambda w: loss(w, x, y, p), lambda w: g_loss(w, x, y, p), epsilon, kmax)
+        elif optimizer == "CGM":
+            self.weights, self.out =  CGM(self.weights, lambda w: loss(w, x, y, p), lambda w: g_loss(w, x, y, p), epsilon, kmax, "FR", 0)
+        elif optimizer == "BFGS":
+            self.weights, self.out = BFGS(self.weights, lambda w: loss(w, x, y, p), lambda w: g_loss(w, x, y, p), epsilon, kmax)
+        else:
+            raise ValueError("Invalid optimizer.")
+        self.x, self.y, self.p = x, y, p
+        self.optimizer = optimizer
+        self._trained = True
 
+    def cvtrain(self, optimizer, x, y, epsilon=10e-6, kmax=1000):
+        Xtr = x[len(x)//5:]
+        ytr = y[len(y)//5:]
+        Xval = x[:len(x)//5]
+        yval = y[:len(y)//5]
 
+        best_lambda = 0
+        best_acc = 0
+        for p in np.arange(0, 3, 0.3):
+            self.weights = np.zeros((1, len(self.weights[0])))
+            self.train(optimizer, Xtr, ytr, p, epsilon, kmax)
+            if self.accuracy(Xval, yval) > best_acc:
+                best_acc = self.accuracy(Xval, yval)
+                best_lambda = p
+        self.train(optimizer, x, y, best_lambda, epsilon, kmax)
 
+    def predict(self, x):
+        return np.round(y(x, self.weights))
 
+    def accuracy(self, x, y):
+        return 100 * np.sum(self.predict(x) == y) / len(y)
+
+    def summary(self, Xte, yte):
+        if not self._trained:
+            print("Model is not trained yet")
+        else:
+            print(f"Single Layer Neural Network (SLNN)")
+            print("-"*75)
+            print(f"Input size:\t\t\t{len(np.squeeze(self.weights))}")
+            print(f"Output size:\t\t\t1 (binary)")
+            print("-"*75)
+            print(f"Train data:\t\t\t{len(self.x)} observations")
+            print(f"Chosen optimization routine:\t{self.optimizer}")
+            print(f"Regularization parameter:\t{self.p}")
+            print(f"Loss:\t\t\t\t{loss(self.weights, self.x, self.y, self.p)}")
+            print(f"Training accuracy:\t\t{self.accuracy(self.x, self.y)}%")
+            print(f"Test accuracy:\t\t\t{self.accuracy(Xte, yte)}%")
+            print("-"*75)
+            print("Gradient: \n", g_loss(self.weights, self.x, self.y, self.p))
+            print("-"*75)
+            num_show(self.weights)
+            return self.out
